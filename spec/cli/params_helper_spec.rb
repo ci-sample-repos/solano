dc84aa54f141547bd1de65300f05a74b4a58ff2b
Vasyl Yaremkiv <vasylyaremkiv@gmail.com>
2017-08-17 11:52:11 +0300

    check

--- spec/cli/params_helper_spec.rb
+++ spec/cli/params_helper_spec.rb
@@ -17,6 +17,8 @@ describe ParamsHelper do
       expect(true).to eq(true)
     end
 
+
+
     it 'return host params if it is present' do
       params = {'host' => 'host.example' }
       expect(self.class.default_host(params)).to eq(params['host'])
